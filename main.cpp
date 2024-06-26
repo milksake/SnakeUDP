bcd91dbe6688404a24bf78dfc63b7ca2ca35801f
milksake <caceresmariana443@gmail.com>
2024-06-25 21:35:59 -0500

    xd

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "server.h"
+#include <cmath>
+#include <typeinfo>
+#include <mutex>
+
+#define PI 3.1416
 
 #define BOARD_WIDTH 40
 #define BOARD_HEIGHT 20
 #define WINNER_POINTS 100
+#define TIME 160
+
+
+std::vector<std::pair<std::string, std::pair<sockaddr_storage, socklen_t>>> toProcess;
+int currx = 0;
 
 struct Snake {
   std::vector<std::pair<int, int>> body;
@@ -12,36 +22,75 @@ struct Snake {
 };
 
 std::map<char, Snake> snakes;
+char OGBoard[BOARD_HEIGHT][BOARD_WIDTH];
 char board[BOARD_HEIGHT][BOARD_WIDTH];
 std::unordered_map<char, sockaddr_storage> clients;
 std::unordered_map<char, socklen_t> clients_sizes;
 
+std::mutex mt;
+
+char& getMap(int y, int x)
+{
+  return OGBoard[y][(x + currx) % BOARD_WIDTH];
+}
+
+void printBoard(char b[BOARD_HEIGHT][BOARD_WIDTH])
+{
+  for (int i = 0; i < BOARD_HEIGHT; i++)
+  {
+    for (int j = 0; j < BOARD_WIDTH; j++)
+      std::cout << b[i][j];
+    std::cout << '\n';
+  }
+}
+
 void initialize_board() {
-  memset(board, ' ', sizeof(board));
+  memset(OGBoard, ' ', sizeof(OGBoard));
+
+  double a = 5.0;
+  double b = 10.0;
+  int offset = 12;
+  for (int i = 0; i < BOARD_WIDTH; i++)
+  {
+    double y = a/2.0f*std::sin(PI/b*i) + a/2.0f;
+    OGBoard[(int)y][i] = '#';
+    OGBoard[BOARD_HEIGHT - 1 - (int)y][(i + offset) % BOARD_WIDTH] = '#';
+  }
+
+  // printBoard(OGBoard);
+
 }
 
 void update_board() {
-  memset(board, ' ', sizeof(board));
+  // std::cout << "UPDATE BEGIN\n";
+  for (int i = 0; i < BOARD_HEIGHT; i++)
+  {
+    for (int j = 0; j < BOARD_WIDTH; j++)
+      board[i][j] = getMap(i, j);
+  }
   for (const auto& pair : snakes) {
     const Snake& snake = pair.second;
     for (const auto &part : snake.body) {
       // std::cout << part.first << " " << part.second << " ";
+      if (part.first < 0 || part.first >= BOARD_HEIGHT || part.second < 0 || part.second >= BOARD_WIDTH)
+        continue;
       board[part.first][part.second] = snake.initial;
       // std::cout << snake.initial << " ";
     }
     // std::cout << std::endl;
   }
+  // std::cout << "UPDATE FIN\n";
 }
 
 bool is_collision(const std::pair<int, int>& head, char initial) {
-    return (board[head.first][head.second] != ' ' && board[head.first][head.second] != initial);
+    return (getMap(head.first,head.second) != ' ' && board[head.first][head.second] != initial);
 }
 
 void bfs_clear_snake(char initial) {
   std::queue<std::pair<int, int>> q;
   for (int i = 0; i < BOARD_HEIGHT; ++i) {
     for (int j = 0; j < BOARD_WIDTH; ++j) {
-      if (board[i][j] == initial) {
+      if (getMap(i,j) == initial) {
         q.push({i, j});
       }
     }
@@ -49,7 +98,7 @@ void bfs_clear_snake(char initial) {
 
   while (!q.empty()) {
       auto [x, y] = q.front(); q.pop();
-      board[x][y] = ' ';
+      getMap(x,y) = ' ';
   }
 }
 
@@ -65,22 +114,32 @@ void move_snake(int socket, char initial, char direction, sockaddr_storage addr,
   auto head = snake.body.front();
   std::pair<int, int> new_head = head;
 
+  bool type;
+  int diff;
   switch (direction) {
   case 'u':
     new_head.first -= 1;
+    type = 0;
+    diff = -1;
     break;
   case 'd':
     new_head.first += 1;
+    type = 0;
+    diff = 1;
     break;
   case 'l':
     new_head.second -= 1;
+    type = 1;
+    diff = -1;
     break;
   case 'r':
     new_head.second += 1;
+    type = 1;
+    diff = 1;
     break;
   }
 
-  if (new_head.first < 0 || new_head.first >= BOARD_HEIGHT || new_head.second < 0 || new_head.second >= BOARD_WIDTH)
+  if (new_head.first < 0 || new_head.first >= BOARD_HEIGHT || new_head.second < 0 || new_head.second >= BOARD_WIDTH || getMap(new_head.first, new_head.second) == '#')
   {
     bfs_clear_snake(initial);
     snakes.erase(initial);
@@ -105,14 +164,27 @@ void move_snake(int socket, char initial, char direction, sockaddr_storage addr,
         message += initial;
         broadcast(socket, message);
       }
+
       return;
     }
 
-    snake.body.insert(snake.body.begin(), new_head);
-    if (snake.body.size() > 5) {
-      snake.body.pop_back();
+    // snake.body.insert(snake.body.begin(), new_head);
+    // if (snake.body.size() > 5) {
+    //   snake.body.pop_back();
+    // }
+    // snake.direction = direction;
+
+    for (auto& seg : snake.body)
+    {
+      if (type)
+      {
+        seg.second += diff;
+      }
+      else
+      {
+        seg.first += diff;
+      }
     }
-    snake.direction = direction;
   }
 }
 
@@ -126,11 +198,14 @@ void handle_initialization(int socket, const std::string& message, sockaddr_stor
     response = "N";
     ini = false;
   } else {
-    std::vector<std::pair<int, int>> body(5, {BOARD_HEIGHT / 2, BOARD_WIDTH / 2});
-    for (int i = 1; i < 5; ++i) {
-      body[i].second -= i;
-    }
-    snakes[initial] = Snake{body, initial, 'U'};
+    std::vector<std::pair<int, int>> body(5);
+    body[0] = {BOARD_HEIGHT / 2, BOARD_WIDTH / 2};
+    body[1] = {BOARD_HEIGHT / 2 + 1, BOARD_WIDTH / 2};
+    body[2] = {BOARD_HEIGHT / 2 - 1, BOARD_WIDTH / 2};
+    body[3] = {BOARD_HEIGHT / 2, BOARD_WIDTH / 2 + 1};
+    body[4] = {BOARD_HEIGHT / 2, BOARD_WIDTH / 2 - 1};
+    auto& uu = snakes[initial];
+    uu = Snake{body, initial, 'U'};
     clients[initial] = addr;
     clients_sizes[initial] = addr_size;
     response = "Y";
@@ -139,13 +214,6 @@ void handle_initialization(int socket, const std::string& message, sockaddr_stor
   if (ini)
   {
     update_board();
-    std::string response = "m";
-    for (int i = 0; i < BOARD_HEIGHT; ++i) {
-      for (int j = 0; j < BOARD_WIDTH; ++j) {
-        response += board[i][j];
-      }
-    }
-    broadcast(socket, response);
   }
 }
 
@@ -153,6 +221,56 @@ void handle_move(int socket, const std::string& message, sockaddr_storage addr, 
   char initial = message[2];
   char direction = message[1];
   move_snake(socket, initial, direction, addr, addr_size);
+  update_board();
+}
+
+void main_handler(int socket, const std::string& datum, sockaddr_storage addr, socklen_t addr_size) {
+  std::cout << datum << std::endl;
+  char message_type = datum[0];
+  if (handlers.find(message_type) != handlers.end()) {
+    toProcess.push_back({datum, {addr, addr_size}});
+    // handlers[message_type](socket, datum, addr, addr_size);
+    // std::cout << "PUSHED\n";
+  }
+}
+
+void update(int socket)
+{
+  currx += 1;
+
+  for (auto& mess : toProcess)
+  {
+    std::cout << mess.first << '\n';
+    char message_type = mess.first[0];
+    handlers[message_type](socket, mess.first, mess.second.first, mess.second.second);
+    std::cout << mess.first << '\n';
+  }
+
+  toProcess.clear();
+
+  std::vector<char> initials_delete;
+
+  for (const auto& pair : snakes) {
+    const Snake& snake = pair.second;
+    auto part = snake.body.front();
+    char initial = snake.initial;
+    if (getMap(part.first, part.second) == '#')
+    {
+      bfs_clear_snake(initial);
+      // snakes.erase(initial);
+      initials_delete.push_back(initial);
+      std::string message = "L";
+      message += initial;
+      sendString(socket, message, clients[initial], clients_sizes[initial]);
+      std::cout << "OUT OF BOUNDS" << std::endl;
+    }
+  }
+
+  for (auto c : initials_delete)
+  {
+    snakes.erase(c);
+  }
+
   update_board();
   std::string response = "m";
   for (int i = 0; i < BOARD_HEIGHT; ++i) {
@@ -163,11 +281,14 @@ void handle_move(int socket, const std::string& message, sockaddr_storage addr, 
   broadcast(socket, response);
 }
 
-void main_handler(int socket, const std::string& datum, sockaddr_storage addr, socklen_t addr_size) {
-  std::cout << datum << std::endl;
-  char message_type = datum[0];
-  if (handlers.find(message_type) != handlers.end()) {
-    handlers[message_type](socket, datum, addr, addr_size);
+void ticker(int socket)
+{
+  while(true)
+  {
+    mt.lock();
+    update(socket);
+    mt.unlock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(TIME));
   }
 }
 
@@ -177,13 +298,11 @@ int main() {
   handlers['I'] = handle_initialization;
   handlers['M'] = handle_move;
 
-  auto handler = [](const int socket, const std::string& datum, sockaddr_storage addr, socklen_t addr_size) {
-    main_handler(socket, datum, addr, addr_size);
-  };
-
-  UDPListener listener(handler, PORT);
+  UDPListener listener(&main_handler, PORT);
 
   listener.run();
+
+  std::thread th(&ticker, listener.getSocketFD());
 
   while (true) {
   }
